efeea9e9d83d4236e0ca7909de37416c5b5ed0d7
GooSam <sunken99quesera@gmail.com>
2025-04-15 18:19:28 +0900

    프로젝트 디폴트 설정 업로드

--- PAProjectZombieCity/Assets/02.CodeCore/CCoreStage/CManagerStageBase.cs
+++ PAProjectZombieCity/Assets/02.CodeCore/CCoreStage/CManagerStageBase.cs
@@ -1,9 +1,45 @@
+using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Events;
 public abstract class CManagerStageBase : CManagerTemplateBase<CManagerStageBase>
 {
-
-    
+    private List<CStageBase> m_listStageInstance = new List<CStageBase>();
     //------------------------------------------------------------
+    protected override void OnUnityAwake()
+    {
+        base.OnUnityAwake();
+        PrivMgrStageInstance();
+    }
 
+    //--------------------------------------------------------------
+    protected void ProtMgrStageLoad(uint hStageID, UnityAction delFinish, params object [] aParams)
+    {
+        PrivMgrStageLoad(hStageID, delFinish, aParams);
+    }
+
+    //-------------------------------------------------------------
+    private void PrivMgrStageInstance()
+    {
+        if (m_listStageInstance.Count == 0)
+        {
+            GetComponentsInChildren(true, m_listStageInstance);
+        }
+    }
+
+
+    private void PrivMgrStageLoad(uint hStageID, UnityAction delFinish, params object[] aParams)
+    {
+        PrivMgrStageInstance();
+
+        int iComplete = 0;
+        for (int i = 0; i < m_listStageInstance.Count; i++)
+        {
+//            m_listStageInstance[i]
+        }
+    }
+
+
+
+    //-------------------------------------------------------------
+    public CManagerStageBase() : base(false) { }
 }
--- PAProjectZombieCity/Assets/02.CodeCore/CCoreStage/CStageBase.cs
+++ PAProjectZombieCity/Assets/02.CodeCore/CCoreStage/CStageBase.cs
@@ -1,16 +1,88 @@
 using UnityEngine;
+using UnityEngine.Events;
 
-public class CStageBase : MonoBehaviour
+public abstract class CStageBase : CMonoBase
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum EStageStatus
     {
-        
+        None,
+        Loading,        // 에셋을 로딩하고 있다.
+        Ready,          // 로딩이 끝나서 스타트 가능 상태이다
+        Start,          // 스테이지가 시작되었다.
+        End,            // 스테이지가 끝난 상태이다. Reset등으로 재시작이 가능하다.
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private uint m_hStageID = 0;   public uint GetStageID() { return m_hStageID; }
+    private EStageStatus m_eStageStatus = EStageStatus.None;
+    //--------------------------------------------------------------------
+    internal void InterStageLoad(uint hStageID, UnityAction delFinish, params object [] aParams)
     {
-        
+        if (m_eStageStatus == EStageStatus.None)
+        {
+            m_hStageID = hStageID;
+            m_eStageStatus = EStageStatus.Loading;
+            OnStageLoad(hStageID, ()=> {
+                m_eStageStatus = EStageStatus.Ready;
+                delFinish?.Invoke();
+            }, aParams);
+        }
+        else
+        {
+            //Error! 이미 로딩 되어 있다.
+        }
     }
+
+    internal void InterStageStart(int iOption)
+    {
+        if (m_eStageStatus == EStageStatus.Ready)
+        {
+            m_eStageStatus = EStageStatus.Start;
+            OnStageStart(iOption);
+        }
+        else
+        {
+            //Error!
+        }
+    }
+
+    internal void InterStageEnd(int iResult)
+    {
+        if (m_eStageStatus == EStageStatus.Start)
+        {
+            m_eStageStatus = EStageStatus.End;
+            OnStageEnd(iResult);
+        }
+        else
+        {
+            //Error!
+        }
+    }
+
+    internal void InterStageReset(int iOption)
+    {
+        if (m_eStageStatus == EStageStatus.End)
+        {
+            m_eStageStatus = EStageStatus.Ready;
+            OnStageReset(iOption);
+        }
+        else
+        {
+            //Error!
+        }
+    }
+
+    internal void InterStageExit()  // 해당 스테이지를 종료하고 언로드 하기 직전
+    {
+        OnStageExit();
+    }
+
+
+
+    //----------------------------------------------------------------------
+    protected virtual void OnStageLoad(uint hStageID, UnityAction delFinish, params object[] aParams) { }
+    protected virtual void OnStageStart(int iOption) { }
+    protected virtual void OnStageEnd(int iResult) { }
+    protected virtual void OnStageReset(int iOption) { }
+    protected virtual void OnStageExit() { }
 }
