9e271292f4b219240da84b210eb50089165162bd
GooSam <sunken99quesera@gmail.com>
2025-04-15 15:06:18 +0900

    프로젝트 디폴트 설정 업로드

---
+++ PAProjectZombieCity/Assets/02.CodeCore/CCoreStage/CManagerStageBase.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public abstract class CManagerStageBase : CManagerTemplateBase<CManagerStageBase>
+{
+
+    
+    //------------------------------------------------------------
+
+}
